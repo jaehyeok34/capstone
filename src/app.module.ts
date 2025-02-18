2ed5cb6dd7b5c0cf40875375f62b959eb5065fd6
jaehyeok34 <hyeok9942@gmail.com>
2025-02-19 00:40:11 +0900

    create branch

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FileUploadModule } from './file-upload';
-
-@Module({
-  imports: [FileUploadModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- src/file-upload/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './file-upload.service';
-export * from './file-upload.controller';
-export * from './file-upload.module';
