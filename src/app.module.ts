3e455280b8863b5021a8f3268029553340c3b0cc
jaehyeok34 <hyeok9942@gmail.com>
2025-02-19 01:17:52 +0900

    port 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FileUploadModule } from './file-upload';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [FileUploadModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
